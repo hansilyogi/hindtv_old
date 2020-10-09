e58aed934f9999cc8d606f6ad30a39368651fbf8
dhanpalpixelbeget <dhanpal@pixelbeget.com>
2020-10-09 12:20:54 +0530

    updated

--- header.php
+++ header.php
@@ -8,8 +8,8 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <!-- <meta value="http://15.206.236.83/api/" id="website-url">  -->
-    <meta value="http://localhost:3000/api/" id="website-url">
+    <meta value="http://15.206.236.83/api/" id="website-url"> 
+    <!-- <meta value="http://localhost:3000/api/" id="website-url"> -->
 
     <!-- <meta value="https://itfutrz-attendance-system.herokuapp.com/api/" id="website-url">     -->
     <meta value="<?php echo $_SESSION['id']; ?>" id="website-token">
--- index.php
+++ index.php
@@ -14,8 +14,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-  <!-- <meta value="http://15.206.236.83/api/" id="website-url">  -->
-  <meta value="http://localhost:3000/api/" id="website-url">
+  <meta value="http://15.206.236.83/api/" id="website-url"> 
+  <!-- <meta value="http://localhost:3000/api/" id="website-url"> -->
 
 
 
